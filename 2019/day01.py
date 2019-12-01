24cade4ef2aee91177db4f79dd9cb3d22bff67dc
iiewenjie <wen20121724@163.com>
2019-12-01 13:26:03 +0800

    2019 day01

---
+++ 2019/day01.py
@@ -0,0 +1,30 @@
+import math
+
+def read_text(filename):
+    data = []
+    with open(filename, 'r') as fin:
+        for line in fin.readlines():
+            line = line.strip('\n')
+            data.append(int(line))
+    return data
+
+def solution1(data):
+    ret = 0
+    for v in data:
+        a = 0
+        while v > 0:
+            v = math.floor(v / 3) - 2
+            if v > 0:
+                a += v
+        # print(a)
+        ret += a
+    print(ret)
+
+def main():
+    filename = 'input.txt'
+    # filename = 'tmp.txt'
+    data = read_text(filename)
+    solution1(data)
+
+if __name__ == "__main__":
+    main()
